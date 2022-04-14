d14f00c39fd2d9a753c7b3bd213c9cf793b73b4a
Adam Mackey <adambmackey@gmail.com>
2022-04-14 18:19:49 -0400

    added alot

--- Public/main.js
+++ Public/main.js
@@ -10,6 +10,7 @@ const destinationInput = document.querySelector('#destination')
 const dateInput = document.querySelector('#date')
 const imageInput = document.querySelector('#img')
 const textInfo = document.querySelector('#sum')
+const displayContainer = document.querySelector('#cardDisplay')
 
 function newLocation(e){
     e.preventDefault()
@@ -19,10 +20,34 @@ function newLocation(e){
         image: imageInput.value,
         description: textInfo.value
     }
-    console.log(body)
+    // console.log(body)
     axios.post('http://localhost:9876/api/location', body)
     .then(res => console.log(res.data))
     .catch(err => console.log(err))
 }
 
+function getLocation() {
+   
+    axios.get('http://localhost:9876/api/location')
+    .then(res => {
+        console.log(res.data);
+        res.data.map(result => {
+        let displayDiv = document.createElement("div");
+        displayDiv.classList.add("card");
+        displayDiv.style.width = "18rem";
+        let placeHolderImg = './No-Image-Placeholder.svg.png'
+        let displayImg = result.image ? result.image : placeHolderImg
+        displayDiv.innerHTML = `
+            <img src='${displayImg}' class="card-img-top" alt="...">
+            <div class='card-body bg-light '>
+            <h3 class='card-title'>${result.destination}</h3>
+            <p class='card-text overflow-hidden'>${result.description}</p>
+            </div>  `;
+        displayContainer.appendChild(displayDiv);
+        })
+    })
+    .catch(err => console.log(err))
+}
+
 form.addEventListener('submit', newLocation)
+getLocation()
--- server/seed.js
+++ server/seed.js
@@ -16,9 +16,8 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
 module.exports = {
   seed: (req, res) => {
     sequelize
-      .query(
-`
-
+      .query(`
+        DROP TABLE IF EXISTS travelJournal;
         CREATE TABLE IF NOT EXISTS travelJournal(
             id SERIAL PRIMARY KEY, 
             destination VARCHAR(40) NOT NULL,
@@ -29,32 +28,16 @@ module.exports = {
         
 
         INSERT INTO travelJournal (destination, date, image, description)
-        VALUES ('Havasupai','July 7, 2010', 'img url', 'It was so fun I would go back'),
-        ('FollyBeach', 'Aug 4, 2018', 'img url', 'It was so fun I got married there'),
-        ('Bartiloche', 'march 4, 2012', 'img url', 'It was a different country'),
-        ('NewYork', 'Feb 10, 2018', 'img url', 'It was so cool id go back there');
+        VALUES ('Havasupai','July 7, 2010', 'https://media.istockphoto.com/photos/havasu-falls-landscape-picture-id183280773?b=1&k=20&m=183280773&s=170667a&w=0&h=kSTAqqt2W96DZadl6I8vqie2XF73QBSTodgFy8ahTvw=', 'It was so fun I would go back'),
+        ('FollyBeach', 'Aug 4, 2018', '', 'It was so fun I got married there'),
+        ('Bartiloche', 'march 4, 2012', '', 'It was a different country'),
+        ('NewYork', 'Feb 10, 2018', 'https://media.istockphoto.com/photos/downtown-manhattan-new-york-jersey-city-golden-hour-sunset-picture-id1304412814?b=1&k=20&m=1304412814&s=170667a&w=0&h=yUrsej_Tz0kQTrHUMBuYn5OJkdIMo6QFiUbTWkw-e40=', 'It was so cool id go back there');
         `)
       .then(() => {
         console.log("DB seeded!");
         res.sendStatus(200);
       })
       .catch((err) => console.log("error seeding DB", err));
-  },
-  postInfo: (req, res) => {
-    sequelize.query(`
-      INSERT INTO travelJournal (destination, date, image, description)
-      VALUES ('${destination}', '${date}', '${image}', '${description}');
-      `)
-      .then(dbRes => res.status(200).send(dbRes[0]))
-      .catch(err => console.log(err))
-  },
-  getInfo: (req, res) => {
-    
-    sequelize.query(`
-    SELECT * FROM travelJournal;
-    `)
-    .then(dbRes => res.status(200).send(dbRes[0]))
-    .catch(err => console.log(err))
   }
 
 
